499e4d721735ce4af9e2099466fcda4f3bec71ab
smmazharul <smmazharulislam20@gmail.com>
2026-01-02 10:29:46 +0600

    first commit on the oop in python class and object

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Person:
+    def show_name(self,name):
+        print(f"My name is {name}")
+mazharul = Person()
+humayra = Person()
+humayra.show_name("Humayra")
+mazharul.show_name("Mazharul")
