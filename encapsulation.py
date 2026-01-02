d86a61fee4a7b81cf298f93fabc7a3b136836bfa
smmazharul <smmazharulislam20@gmail.com>
2026-01-02 19:09:58 +0600

    learn how to get and set encapsulation data means hide data

--- encapsulation.py
+++ encapsulation.py
@@ -1,13 +1,20 @@
 class Car:
-    def __init__(self,name,color,mileage):
-        self.name = name 
+    def __init__(self, name, color, mileage):
+        self.name = name
         self.color = color
         self.__mileage = mileage
-    def show_details(self):
-        print(f"Name: {self.name}, Color: {self.color}, Mileage:{self.__mileage}")
+    def get_mileage(self):
+        return self.__mileage
+    def set_mileage(self,new_mileage):
+        self.__mileage = new_mileage
+
+BMW = Car("MBW", "black", 50)
+BMW.name = "sdsdkhds"
+BMW.set_mileage(20)
+print(BMW.name)
+print(BMW.get_mileage())
 
 
-BMW = Car("BMW", "Black", 50)
-BMW.show_details()
+
         
        
