5cc3ef988d64d5ab4bf2907ac7eb1a79cfa7cd94
smmazharul <smmazharulislam20@gmail.com>
2026-01-04 20:18:13 +0600

    when we use abstrac class then the parent class make a rule and all child class need follow that rule but

--- abstract.py
+++ abstract.py
@@ -4,17 +4,26 @@ class Vehicle(ABC):
     @abstractmethod
     def start(self):
         pass
+    @abstractmethod
+    def stop(self):
+        pass
 
 class Car (Vehicle):
     def start(self):
         print("Car started")
+    def stop(self):
+        print("Car stoped")
 
 class Bike(Vehicle):
     def start(self):
         print("Bike started")
+    def stop(self):
+        print("Bike stoped")
 
 car = Car()
 bike = Bike()
 
 car.start()
+bike.stop()
+car.stop()
 bike.start()
