6953661a647adc80cecdd05d48ff8c86f4be808e
smmazharul <smmazharulislam20@gmail.com>
2026-01-05 14:49:37 +0600

    reacap list in python with 20 practice problem

--- abstract.py
+++ abstract.py
@@ -1,29 +1,65 @@
+# from abc import ABC, abstractmethod
+
+# class Vehicle(ABC):
+#     @abstractmethod
+#     def start(self):
+#         pass
+#     @abstractmethod
+#     def stop(self):
+#         pass
+
+# class Car (Vehicle):
+#     def start(self):
+#         print("Car started")
+#     def stop(self):
+#         print("Car stoped")
+
+# class Bike(Vehicle):
+#     def start(self):
+#         print("Bike started")
+#     def stop(self):
+#         print("Bike stoped")
+
+# car = Car()
+# bike = Bike()
+
+# car.start()
+# bike.stop()
+# car.stop()
+# bike.start()
+
+
+#================== Shape Area Calculation===============
+
+# from abc import ABC, abstractmethod
+
+# class Shape(ABC):
+#     @abstractmethod
+#     def area(self):
+#         pass
+# class Regtangle(Shape):
+#     def area (self,length, width):
+#         self.length= length
+#         self.width = width
+#         print(f"Area = {self.length * self.width}")
+
+
+    
+# r = Regtangle()
+# r.area(2,3)
+
+"""================== Bank System ==============="""
+
 from abc import ABC, abstractmethod
 
-class Vehicle(ABC):
+class Bank(ABC):
     @abstractmethod
-    def start(self):
-        pass
-    @abstractmethod
-    def stop(self):
+    def interest_rate(self):
         pass
 
-class Car (Vehicle):
-    def start(self):
-        print("Car started")
-    def stop(self):
-        print("Car stoped")
+class BrackBank(Bank):
+    def interest_rate(self):
+        print("Interest Rate is 7 %")
 
-class Bike(Vehicle):
-    def start(self):
-        print("Bike started")
-    def stop(self):
-        print("Bike stoped")
-
-car = Car()
-bike = Bike()
-
-car.start()
-bike.stop()
-car.stop()
-bike.start()
+b1 = BrackBank()
+b1.interest_rate()
---
+++ recaplist.py
@@ -0,0 +1,3 @@
+prices = [50,20,120,30,80]
+total = sum(prices)
+print(total)
